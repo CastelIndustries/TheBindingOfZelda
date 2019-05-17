fc3297c839164386bf8a988fa7c990e25787ce4e
frangi0 <mrmonkeex@gmail.com>
2019-05-17 10:14:13 +0200

    Added test 2

---
+++ TheBindingOfZeldaTests/cmake-build-debug/CMakeFiles/TheBindingOfZeldaTests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheBindingOfZeldaTests.dir/main.cpp.o"
+  "TheBindingOfZeldaTests.pdb"
+  "TheBindingOfZeldaTests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheBindingOfZeldaTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
