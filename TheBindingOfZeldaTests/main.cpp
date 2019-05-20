6d7c8e00f2719aeaa82a5d1383bfd203c1e46b06
nicola <nicoruggi97@live.it>
2019-05-20 15:18:40 +0200

    Added te first Test for Google test, it works successfully, but still have some problems with github in decide what ignore in the file "gitignore".

--- TheBindingOfZeldaTests/main.cpp
+++ TheBindingOfZeldaTests/main.cpp
@@ -10,12 +10,34 @@
 #include "googletest/include/gtest/gtest.h"
 
 
-TEST(Collisions, CollisionsTests) {
+TEST(Collisions, CharactersColliding) {
+    CharacterFactory characterFactory;
+    sf::Texture skeletonTexture;
+    std::unique_ptr<Character> skeleton = characterFactory.Create(type::SKELETON, &skeletonTexture, sf::Vector2u(3, 4),
+                                                                  0.2f, 300.0f);
+    sf::Texture ghostTexture;
+    std::unique_ptr<Character> ghost = characterFactory.Create(type::GHOST, &ghostTexture, sf::Vector2u(3, 4), 0.1f,
+                                                               200.f);
 
+    skeleton->body.setPosition(sf::Vector2f(50, 50));
+    ghost->body.setPosition(sf::Vector2f(50, 50));
+
+    EXPECT_EQ(true, skeleton->GetCollider().CheckCollision(ghost->GetCollider(), 0.0f));
 }
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(1211, 865), "The Binding of Zelda", sf::Style::Close);
+TEST(Try, Sum) {
+    int a = 0;
+    int b = 0;
+    int c = a + b;
+    EXPECT_EQ(0, c);
+}
+
+int main(int argc, char *argv[]) {
+
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+    /*sf::RenderWindow window(sf::VideoMode(1211, 865), "The Binding of Zelda", sf::Style::Close);
     sf::Texture playerTexture;
     sf::Texture rabbitTexture;
     sf::Texture skeletonTexture;
@@ -28,12 +50,12 @@ int main() {
 
     characterList.push_back(characterFactory.Create(type::PLAYER, &playerTexture, sf::Vector2u(3, 4), 0.1f, 300.0f));
     characterList.push_back(characterFactory.Create(type::RABBIT, &rabbitTexture, sf::Vector2u(6, 4), 0.1f, 200.0f));
-    characterList.push_back(
-            characterFactory.Create(type::SKELETON, &skeletonTexture, sf::Vector2u(3, 4), 0.2f, 300.0f));
+    characterList.push_back(characterFactory.Create(type::SKELETON, &skeletonTexture, sf::Vector2u(3, 4), 0.2f, 300.0f));
     characterList.push_back(characterFactory.Create(type::GHOST, &ghostTexture, sf::Vector2u(3, 4), 0.1f, 200.f));
     window.clear();
 
+*/
 
 
-    return 0;
+
 }
