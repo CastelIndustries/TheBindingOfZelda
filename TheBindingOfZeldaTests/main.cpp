db288220373d11632a067fed779e3b9ce1527b14
nicola <nicoruggi97@live.it>
2019-06-06 15:25:28 +0200

    Added te first Test for Google test, it works successfully, but still have some problems with github in decide what ignore in the file "gitignore".

--- TheBindingOfZeldaTests/main.cpp
+++ TheBindingOfZeldaTests/main.cpp
@@ -7,17 +7,18 @@
 #include "iostream"
 #include <list>
 #include "../CharacterFactory.h"
+#include "../DEFINITIONS.h"
 #include "googletest/include/gtest/gtest.h"
 
 
 TEST(Collisions, CharactersColliding) {
     CharacterFactory characterFactory;
-    sf::Texture skeletonTexture;
-    std::unique_ptr<Character> skeleton = characterFactory.Create(type::SKELETON, &skeletonTexture, sf::Vector2u(3, 4),
-                                                                  0.2f, 300.0f);
-    sf::Texture ghostTexture;
-    std::unique_ptr<Character> ghost = characterFactory.Create(type::GHOST, &ghostTexture, sf::Vector2u(3, 4), 0.1f,
-                                                               200.f);
+
+    std::unique_ptr<Character> skeleton = characterFactory.Create(type::SKELETON, SKELETON_TEXTURE,
+                                                                  SKELETON_IMAGE_COUNT, SKELETON_SWITCH_TIME,
+                                                                  SKELETON_SPEED);
+    std::unique_ptr<Character> ghost = characterFactory.Create(type::GHOST, GHOST_TEXTURE, GHOST_IMAGE_COUNT,
+                                                               GHOST_SWITCH_TIME, GHOST_SPEED);
 
     skeleton->body.setPosition(sf::Vector2f(50, 50));
     ghost->body.setPosition(sf::Vector2f(50, 50));
@@ -25,37 +26,76 @@ TEST(Collisions, CharactersColliding) {
     EXPECT_EQ(true, skeleton->GetCollider().CheckCollision(ghost->GetCollider(), 0.0f));
 }
 
-TEST(Try, Sum) {
-    int a = 0;
-    int b = 0;
-    int c = a + b;
-    EXPECT_EQ(0, c);
+TEST(Damaging, Skeleton_damaging) {
+    CharacterFactory characterFactory;
+
+    std::unique_ptr<Character> skeleton = characterFactory.Create(type::SKELETON, SKELETON_TEXTURE,
+                                                                  SKELETON_IMAGE_COUNT, SKELETON_SWITCH_TIME,
+                                                                  SKELETON_SPEED);
+
+
+    skeleton->body.setPosition(100, 0);
+    Bullet newBullet("tibi", sf::Vector2f(30, 30), sf::Vector2f(1.0f, 0.0f));
+    newBullet.setPos(sf::Vector2f(0.0f, 0.0f));
+    newBullet.fire(30.0f);
+    if (newBullet.GetCollider().CheckCollision(skeleton->GetCollider(), 0.0f)) {
+        skeleton->hp -= newBullet.damage;
+    }
+
+    EXPECT_EQ(100 - newBullet.damage, skeleton->hp);
 }
 
+TEST(Damaging, Damaging_Skeleton_From_Left) {
+    CharacterFactory characterFactory;
+
+    std::unique_ptr<Character> skeleton = characterFactory.Create(type::SKELETON, SKELETON_TEXTURE,
+                                                                  SKELETON_IMAGE_COUNT, SKELETON_SWITCH_TIME,
+                                                                  SKELETON_SPEED);
+    std::unique_ptr<Character> player = characterFactory.Create(type::PLAYER, PLAYER_TEXTURE, PLAYER_IMAGE_COUNT,
+                                                                PLAYER_SWITCH_TIME, PLAYER_SPEED);
+
+
+    skeleton->body.setPosition(100, 0);
+    Bullet newBullet("tibi", sf::Vector2f(30, 30), skeleton->playerDir);
+
+    player->body.setPosition(0.0f, 0.0f);
+    newBullet.setPos(sf::Vector2f(player->body.getPosition().x + player->body.getSize().x / 2,
+                                  player->body.getPosition().y + player->body.getSize().y / 2));
+
+    player->BulletVec.push_back(newBullet);
+    player->BulletVec[0].fire(30.0f);
+    if (player->BulletVec[0].GetCollider().CheckCollision(skeleton->GetCollider(), 0.0f)) {
+        skeleton->hp -= newBullet.damage;
+    }
+    EXPECT_EQ(0, skeleton->hp);
+}
+
+TEST(Damaging, Player_damaging_from_left) {
+    CharacterFactory characterFactory;
+    std::unique_ptr<Character> skeleton = characterFactory.Create(type::SKELETON, SKELETON_TEXTURE,
+                                                                  SKELETON_IMAGE_COUNT, SKELETON_SWITCH_TIME,
+                                                                  SKELETON_SPEED);
+    std::unique_ptr<Character> player = characterFactory.Create(type::PLAYER, PLAYER_TEXTURE, PLAYER_IMAGE_COUNT,
+                                                                PLAYER_SWITCH_TIME, PLAYER_SPEED);
+
+
+    Bullet newBullet("tibi", sf::Vector2f(30, 30), player->playerDir);
+    player->body.setPosition(100.0f, 0.0f);
+    skeleton->body.setPosition(0.0f, 0.0f);
+    newBullet.setPos(sf::Vector2f(skeleton->body.getPosition().x + skeleton->body.getSize().x / 2,
+                                  skeleton->body.getPosition().y + skeleton->body.getSize().y / 2));
+
+    skeleton->BulletVec.push_back(newBullet);
+    skeleton->BulletVec[0].fire(30.0f);
+    if (skeleton->BulletVec[0].GetCollider().CheckCollision(player->GetCollider(), 0.0f)) {
+        player->hp -= newBullet.damage;
+    }
+    EXPECT_EQ(0, player->hp);
+
+}
 int main(int argc, char *argv[]) {
 
     testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
-    /*sf::RenderWindow window(sf::VideoMode(1211, 865), "The Binding of Zelda", sf::Style::Close);
-    sf::Texture playerTexture;
-    sf::Texture rabbitTexture;
-    sf::Texture skeletonTexture;
-    sf::Texture ghostTexture;
-    sf::Clock clock;
-    float deltaTime = clock.restart().asSeconds();
-
-    std::list<std::unique_ptr<Character>> characterList;
-    CharacterFactory characterFactory;
-
-    characterList.push_back(characterFactory.Create(type::PLAYER, &playerTexture, sf::Vector2u(3, 4), 0.1f, 300.0f));
-    characterList.push_back(characterFactory.Create(type::RABBIT, &rabbitTexture, sf::Vector2u(6, 4), 0.1f, 200.0f));
-    characterList.push_back(characterFactory.Create(type::SKELETON, &skeletonTexture, sf::Vector2u(3, 4), 0.2f, 300.0f));
-    characterList.push_back(characterFactory.Create(type::GHOST, &ghostTexture, sf::Vector2u(3, 4), 0.1f, 200.f));
-    window.clear();
-
-*/
-
-
-
 }
